56830abfcb70d4ad8d85a18f7df33c10ee9b4481
Loïc Boutet <loic@boutet.com>
2014-06-22 20:30:17 +0200

    adding import of gmail contact

---
+++ config/initializers/omnicontacts.rb
@@ -0,0 +1,8 @@
+require "omnicontacts"
+
+Rails.application.middleware.use OmniContacts::Builder do
+  importer :gmail, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {:redirect_path => "/invites/gmail/contact_callback"}
+#  importer :yahoo, "consumer_id", "consumer_secret", {:callback_path => '/callback'}
+#  importer :hotmail, "client_id", "client_secret"
+#  importer :facebook, "client_id", "client_secret"
+end
