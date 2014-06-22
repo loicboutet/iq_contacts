c86d68de194ff138e931cffd80d0e7776462679a
Loïc Boutet <loic@boutet.com>
2014-06-22 23:26:24 +0200

    trying to fix omnicontact

--- config/initializers/omnicontacts.rb
+++ config/initializers/omnicontacts.rb
@@ -1,8 +1,5 @@
 require "omnicontacts"
 
 Rails.application.middleware.use OmniContacts::Builder do
-  importer :gmail, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {:redirect_path => "/invites/gmail/contact_callback"}, :max_results => 1000
-#  importer :yahoo, "consumer_id", "consumer_secret", {:callback_path => '/callback'}
-#  importer :hotmail, "client_id", "client_secret"
-#  importer :facebook, "client_id", "client_secret"
+  importer :gmail, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {:redirect_path => "/invites/gmail/contact_callback"}
 end
