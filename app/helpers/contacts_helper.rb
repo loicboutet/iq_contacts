abdfee96d38f3238da7eca0dbd4d34ba061f1b40
Loïc Boutet <loic@boutet.com>
2014-06-25 01:35:14 +0200

    fadding locale to the cache key

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -2,7 +2,7 @@ module ContactsHelper
 
   def cache_key_with_format(model, format)
     return "" if model.blank?
-    return model.cache_key.to_s + format.to_s
+    return model.cache_key.to_s + format.to_s + I18n.locale
   end
 
 end
