023b8ee639374025502c2f081e77c02ba443fd20
Loïc Boutet <loic@boutet.com>
2014-06-25 00:55:09 +0200

    fixing bug on cache

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -2,7 +2,7 @@ module ContactsHelper
 
   def cache_key_with_format(model, format)
     return "" if model.blank?
-    model.cache_key.to_s + format.to_s
+    return model.cache_key.to_s + format.to_s
   end
 
 end
